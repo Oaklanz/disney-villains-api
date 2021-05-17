d76077f9d297126ff78afdec6b6ae147cb9b1b0c
Oaklanz <69703980+Oaklanz@users.noreply.github.com>
2021-05-16 20:42:08 -0400

    created a mocks folder and a villains.js file

--- tests/controllers/villains.test.js
+++ tests/controllers/villains.test.js
@@ -3,13 +3,21 @@ const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
 const {describe, it} = require('mocha')
 const { getAllVillains, getVillainBySlug, saveNewVillain } = require('../../controller/villains.js')
+const moedels = require('../../models')
+const { stub } = require('sinon')
 
 chai.use(sinonChai)
 const { expect } = chai
 
 describe('villains Controller', () => {
 describe('Get  All Villains', () ={
+it('gets list of villains from DB and calls respond.send() with that list', async () => {
+    const stubbedFindAll = sinon.stub(models.villains,'findAll')
+    const stubbedSend = sinon.stub()
+    const response  = {send: stubbedSend}
 
+    await getAllVillains({}, response)
+} )
 })
 
 describe('Get Villain By Slug', () =>{
