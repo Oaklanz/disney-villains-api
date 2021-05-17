8e6558ed1a17302f55c3a809bbae702308b388b5
Oaklanz <69703980+Oaklanz@users.noreply.github.com>
2021-05-16 22:49:44 -0400

    test suite for getVillainBySlug passed

--- tests/controllers/villains.test.js
+++ tests/controllers/villains.test.js
@@ -5,7 +5,7 @@ const { describe, it } = require('mocha')
 const { getAllVillains, getVillainBySlug, saveNewVillain } = require('../../controller/villains.js')
 const models = require('../../models')
 // const { stub } = require('sinon')
-const { villainsList } = require('../mocks/villains')
+const { villainsList, singleVillain } = require('../mocks/villains')
 
 chai.use(sinonChai)
 const { expect } = chai
@@ -25,10 +25,14 @@ describe('villains Controller', () => {
 
   describe('Get Villain By Slug', () => {
     it('get a single villain associated with the slug in the DB and calls response.send() with it', async () => {
+      const stubbedFindOne = sinon.stub(models.villains, 'findOne').returns(singleVillain)
       const request = { params: { slug: 'gaston' } }
-
+      const stubbedSend = sinon.stub()
+      const response = { send: stubbedSend }
 
       await getVillainBySlug(request, response)
+    expect(stubbedFindOne).to.have.been.calledWith({ where: { slug: 'gaston' } })
+      expect(stubbedSend).to.have.been.calledWith(singleVillain)
     })
   })
 
