50089be538f4566087213d397b490c649d08031c
Oaklanz <69703980+Oaklanz@users.noreply.github.com>
2021-05-16 20:22:44 -0400

    created all the buckets for my test functions

--- tests/controllers/villains.test.js
+++ tests/controllers/villains.test.js
@@ -3,3 +3,22 @@ const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
 const {describe, it} = require('mocha')
 const { getAllVillains, getVillainBySlug, saveNewVillain } = require('../../controller/villains.js')
+
+chai.use(sinonChai)
+const { expect } = chai
+
+describe('villains Controller', () => {
+describe('Get  All Villains', () ={
+
+})
+
+describe('Get Villain By Slug', () =>{
+
+})
+
+describe('Save New Villain', () => {
+
+})
+
+})
+
