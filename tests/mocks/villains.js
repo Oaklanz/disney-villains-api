36bf68b3550160c894f4ce724b3d853873828ee4
Oaklanz <69703980+Oaklanz@users.noreply.github.com>
2021-05-16 20:59:54 -0400

    populated my  villains.js file in mocks folder with a villainsList[] containing a mock data

---
+++ tests/mocks/villains.js
@@ -0,0 +1,13 @@
+const villainsList = [{
+  name: 'Captain Hook',
+  movie: 'Peter Pan',
+  slug: 'captain-hook',
+},
+{
+  name: 'Cruella de Vil',
+  movie: 'One Hundred and One Dalmatians',
+  slug: 'cruella-de-vil',
+}]
+
+
+modules.exports = { villainsList }
